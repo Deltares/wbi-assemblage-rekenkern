15c0a704d620e9a1199a916260256b48ae231fde
Pieter van Geer <Pieter.vangeer@deltares.nl>
2018-09-13 13:47:49 +0200

    ASK-12 Correct version information

--- src/assembly.kernel/Properties/AssemblyInfo.cs
+++ src/assembly.kernel/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@
 [assembly: System.Reflection.AssemblyCompany("Informatiehuis Water")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2018, All rights reserved")]
 [assembly: System.Reflection.AssemblyVersion("1.2.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.0 - snapshot")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.0")]
 
 
