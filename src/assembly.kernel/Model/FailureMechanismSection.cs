16aac55e3c736857df0200e1c450d0e7df06f2bf
DIRECTORY\geer <Pieter.vangeer@deltares.nl>
2019-10-21 16:15:26 +0200

    ASK-15 Change all license headers.

--- src/assembly.kernel/Model/FailureMechanismSection.cs
+++ src/assembly.kernel/Model/FailureMechanismSection.cs
@@ -1,26 +1,24 @@
-﻿#region Copyright (c) 2018 Technolution BV. All Rights Reserved. 
-
-// // Copyright (C) Technolution BV. 2018. All rights reserved.
-// //
-// // This file is part of the Assembly kernel.
-// //
-// // Assembly kernel is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU Lesser General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
-// // 
-// // This program is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// // GNU Lesser General Public License for more details.
-// //
-// // You should have received a copy of the GNU Lesser General Public License
-// // along with this program. If not, see <http://www.gnu.org/licenses/>.
-// //
-// // All names, logos, and references to "Technolution BV" are registered trademarks of
-// // Technolution BV and remain full property of Technolution BV at all times.
-// // All rights reserved.
-
+﻿#region Copyright (C) Rijkswaterstaat 2019. All rights reserved
+// Copyright (C) Rijkswaterstaat 2019. All rights reserved.
+//
+// This file is part of the Assembly kernel.
+//
+// Assembly kernel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Rijkswaterstaat" are registered trademarks of
+// Rijkswaterstaat and remain full property of Rijkswaterstaat at all times.
+// All rights reserved.
 #endregion
 
 using Assembly.Kernel.Exceptions;
