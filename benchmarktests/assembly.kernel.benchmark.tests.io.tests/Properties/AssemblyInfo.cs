1fc3fb51f92f064421f572c7479a18e1f79d8ead
DIRECTORY\geer <Pieter.vangeer@deltares.nl>
2022-04-22 16:23:11 +0200

    ASK-56 Update AssemblyInfos

--- benchmarktests/assembly.kernel.benchmark.tests.io.tests/Properties/AssemblyInfo.cs
+++ benchmarktests/assembly.kernel.benchmark.tests.io.tests/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("assembly.kernel.benchmark.tests.io.tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rijkswaterstaat")]
 [assembly: AssemblyProduct("assembly.kernel.benchmark.tests.io.tests")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
--- benchmarktests/assembly.kernel.benchmark.tests/Properties/AssemblyInfo.cs
+++ benchmarktests/assembly.kernel.benchmark.tests/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("assembly.kernel.benchmark.tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rijkswaterstaat")]
 [assembly: AssemblyProduct("assembly.kernel.benchmark.tests")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
