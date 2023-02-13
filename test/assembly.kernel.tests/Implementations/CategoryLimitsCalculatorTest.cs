5c65d859d1f36df00b718804e68ca57c59bb9f0d
Robert Tangerman <robert.tangerman@deltares.nl>
2023-02-13 10:54:37 +0100

    Cleanup CategoryLimitsCalculator. ASK-83

--- test/assembly.kernel.tests/Implementations/CategoryLimitsCalculatorTest.cs
+++ test/assembly.kernel.tests/Implementations/CategoryLimitsCalculatorTest.cs
@@ -19,6 +19,7 @@
 // Rijkswaterstaat and remain full property of Rijkswaterstaat at all times.
 // All rights reserved.
 
+using System.Collections.Generic;
 using Assembly.Kernel.Implementations;
 using Assembly.Kernel.Interfaces;
 using Assembly.Kernel.Model;
@@ -31,102 +32,98 @@ namespace Assembly.Kernel.Tests.Implementations
     [TestFixture]
     public class CategoryLimitsCalculatorTest
     {
-        private ICategoryLimitsCalculator categoryLimitsCalculator;
-
-        [SetUp]
-        public void Init()
+        [Test]
+        public void Constructor_ExpectedValues()
         {
-            categoryLimitsCalculator = new CategoryLimitsCalculator();
+            // Call
+            var calculator = new CategoryLimitsCalculator();
+
+            // Assert
+            Assert.IsInstanceOf<ICategoryLimitsCalculator>(calculator);
         }
 
         [Test]
-        [TestCase(0.0003,0.034)]
-        [TestCase(0.00003, 0.0003)]
-        public void CalculateBoi01FunctionalTest(double signalFloodingProbability, double maximumAllowableFloodingProbability)
+        [TestCaseSource(nameof(GetInterpretationCategoryCases))]
+        public void CalculateInterpretationCategoryLimitsBoi01_Always_ReturnsExpectedCategories(
+            AssessmentSection assessmentSection, InterpretationCategory[] expectedCategories)
         {
-            var section = new AssessmentSection((Probability) signalFloodingProbability, (Probability) maximumAllowableFloodingProbability);
-            var results = categoryLimitsCalculator.CalculateInterpretationCategoryLimitsBoi01(section);
+            // Setup
+            var calculator = new CategoryLimitsCalculator();
 
-            Assert.AreEqual(7, results.Categories.Length);
-            InterpretationCategory[] expectedCategories = 
-            {
-                new InterpretationCategory(EInterpretationCategory.III, (Probability) 0.0, (Probability) (signalFloodingProbability / 1000.0)),
-                new InterpretationCategory(EInterpretationCategory.II, (Probability) (signalFloodingProbability / 1000.0), (Probability) (signalFloodingProbability / 100.0)),
-                new InterpretationCategory(EInterpretationCategory.I, (Probability) (signalFloodingProbability / 100.0), (Probability) (signalFloodingProbability / 10.0)),
-                new InterpretationCategory(EInterpretationCategory.Zero, (Probability) (signalFloodingProbability / 10.0), (Probability) signalFloodingProbability),
-                new InterpretationCategory(EInterpretationCategory.IMin, (Probability) signalFloodingProbability, (Probability) maximumAllowableFloodingProbability),
-                new InterpretationCategory(EInterpretationCategory.IIMin, (Probability) maximumAllowableFloodingProbability, (Probability) maximumAllowableFloodingProbability*10.0),
-                new InterpretationCategory(EInterpretationCategory.IIIMin, (Probability) maximumAllowableFloodingProbability*10.0, (Probability)1.0)
-            };
-            CollectionAssert.AreEqual(results.Categories, expectedCategories,new CategoryLimitsEqualityComparer());
+            // Call
+            CategoriesList<InterpretationCategory> categories = calculator.CalculateInterpretationCategoryLimitsBoi01(assessmentSection);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedCategories, categories.Categories, new CategoryLimitsEqualityComparer());
         }
 
         [Test]
-        public void CalculateBoi01CapToOneTest()
+        [TestCaseSource(nameof(GetAssessmentSectionCategoryCases))]
+        public void CalculateAssessmentSectionCategoryLimitsBoi21_Always_ReturnsExpectedCategories(
+            AssessmentSection assessmentSection, AssessmentSectionCategory[] expectedCategories)
         {
-            var signalFloodingProbability = 0.001;
-            var maximumAllowableFloodingProbability = 0.5;
-            var section = new AssessmentSection((Probability)signalFloodingProbability, (Probability)maximumAllowableFloodingProbability);
-            var results = categoryLimitsCalculator.CalculateInterpretationCategoryLimitsBoi01(section);
+            // Setup
+            var calculator = new CategoryLimitsCalculator();
 
-            Assert.AreEqual(7, results.Categories.Length);
+            // Call
+            CategoriesList<AssessmentSectionCategory> categories = calculator.CalculateAssessmentSectionCategoryLimitsBoi21(assessmentSection);
 
-            InterpretationCategory[] expectedCategories =
-            {
-                new InterpretationCategory(EInterpretationCategory.III, (Probability) 0.0, (Probability) (signalFloodingProbability / 1000.0)),
-                new InterpretationCategory(EInterpretationCategory.II, (Probability) (signalFloodingProbability / 1000.0), (Probability) (signalFloodingProbability / 100.0)),
-                new InterpretationCategory(EInterpretationCategory.I, (Probability) (signalFloodingProbability / 100.0), (Probability) (signalFloodingProbability / 10.0)),
-                new InterpretationCategory(EInterpretationCategory.Zero, (Probability) (signalFloodingProbability / 10.0), (Probability) signalFloodingProbability),
-                new InterpretationCategory(EInterpretationCategory.IMin, (Probability) signalFloodingProbability, (Probability) maximumAllowableFloodingProbability),
-                new InterpretationCategory(EInterpretationCategory.IIMin, (Probability) maximumAllowableFloodingProbability, (Probability)1.0),
-                new InterpretationCategory(EInterpretationCategory.IIIMin, (Probability)1.0, (Probability)1.0)
-            };
-            CollectionAssert.AreEqual(results.Categories, expectedCategories, new CategoryLimitsEqualityComparer());
+            // Assert
+            CollectionAssert.AreEqual(expectedCategories, categories.Categories, new CategoryLimitsEqualityComparer());
         }
 
-        [Test]
-        public void CalculateBoi21FunctionalTest()
+        private static IEnumerable<TestCaseData> GetInterpretationCategoryCases()
         {
-            var section = new AssessmentSection(new Probability(0.003), new Probability(0.03));
-            var results = categoryLimitsCalculator.CalculateAssessmentSectionCategoryLimitsBoi21(section);
+            yield return new TestCaseData(
+                new AssessmentSection((Probability) 0.0003, (Probability) 0.034),
+                new[]
+                {
+                    new InterpretationCategory(EInterpretationCategory.III, (Probability) 0.0, (Probability) 0.0000003),
+                    new InterpretationCategory(EInterpretationCategory.II, (Probability) 0.0000003, (Probability) 0.000003),
+                    new InterpretationCategory(EInterpretationCategory.I, (Probability) 0.000003, (Probability) 0.00003),
+                    new InterpretationCategory(EInterpretationCategory.Zero, (Probability) 0.00003, (Probability) 0.0003),
+                    new InterpretationCategory(EInterpretationCategory.IMin, (Probability) 0.0003, (Probability) 0.034),
+                    new InterpretationCategory(EInterpretationCategory.IIMin, (Probability) 0.034, (Probability) 0.34),
+                    new InterpretationCategory(EInterpretationCategory.IIIMin, (Probability) 0.34, (Probability) 1.0)
+                });
 
-            Assert.AreEqual(5, results.Categories.Length);
-
-            AssessmentSectionCategory[] expectedCategories =
-            {
-                new AssessmentSectionCategory(EAssessmentGrade.APlus, (Probability) 0.0, (Probability) 0.0001 ),
-                new AssessmentSectionCategory(EAssessmentGrade.A, (Probability) 0.0001 , (Probability) 0.003 ),
-                new AssessmentSectionCategory(EAssessmentGrade.B, (Probability) 0.003, (Probability) 0.03),
-                new AssessmentSectionCategory(EAssessmentGrade.C, (Probability) 0.03, (Probability) 0.9),
-                new AssessmentSectionCategory(EAssessmentGrade.D, (Probability) 0.9, (Probability) 1.0)
-            };
-
-            CollectionAssert.AreEqual(results.Categories, expectedCategories, new CategoryLimitsEqualityComparer());
+            yield return new TestCaseData(
+                new AssessmentSection((Probability) 0.001, (Probability) 0.5),
+                new[]
+                {
+                    new InterpretationCategory(EInterpretationCategory.III, (Probability) 0.0, (Probability) 0.000001),
+                    new InterpretationCategory(EInterpretationCategory.II, (Probability) 0.000001, (Probability) 0.00001),
+                    new InterpretationCategory(EInterpretationCategory.I, (Probability) 0.00001, (Probability) 0.0001),
+                    new InterpretationCategory(EInterpretationCategory.Zero, (Probability) 0.0001, (Probability) 0.001),
+                    new InterpretationCategory(EInterpretationCategory.IMin, (Probability) 0.001, (Probability) 0.5),
+                    new InterpretationCategory(EInterpretationCategory.IIMin, (Probability) 0.5, (Probability) 1.0),
+                    new InterpretationCategory(EInterpretationCategory.IIIMin, (Probability) 1.0, (Probability) 1.0)
+                });
         }
 
-        [Test]
-        public void CalculateBoi21CapToOneTest()
+        private static IEnumerable<TestCaseData> GetAssessmentSectionCategoryCases()
         {
-            var signalFloodingProbability = new Probability(0.003);
-            var maximumAllowableFloodingProbability = new Probability(0.034);
+            yield return new TestCaseData(
+                new AssessmentSection((Probability) 0.003, (Probability) 0.03),
+                new[]
+                {
+                    new AssessmentSectionCategory(EAssessmentGrade.APlus, (Probability) 0.0, (Probability) 0.0001),
+                    new AssessmentSectionCategory(EAssessmentGrade.A, (Probability) 0.0001, (Probability) 0.003),
+                    new AssessmentSectionCategory(EAssessmentGrade.B, (Probability) 0.003, (Probability) 0.03),
+                    new AssessmentSectionCategory(EAssessmentGrade.C, (Probability) 0.03, (Probability) 0.9),
+                    new AssessmentSectionCategory(EAssessmentGrade.D, (Probability) 0.9, (Probability) 1.0)
+                });
 
-            var section = new AssessmentSection(signalFloodingProbability, maximumAllowableFloodingProbability);
-
-            CategoriesList<AssessmentSectionCategory> results =
-                categoryLimitsCalculator.CalculateAssessmentSectionCategoryLimitsBoi21(section);
-
-            Assert.AreEqual(5, results.Categories.Length);
-
-            AssessmentSectionCategory[] expectedCategories =
-            {
-                new AssessmentSectionCategory(EAssessmentGrade.APlus, (Probability) 0.0, (Probability) 0.0001 ),
-                new AssessmentSectionCategory(EAssessmentGrade.A, (Probability) 0.0001 , (Probability) 0.003 ),
-                new AssessmentSectionCategory(EAssessmentGrade.B, (Probability) 0.003, (Probability) 0.034),
-                new AssessmentSectionCategory(EAssessmentGrade.C, (Probability) 0.034, (Probability) 1.0),
-                new AssessmentSectionCategory(EAssessmentGrade.D, (Probability) 1.0, (Probability) 1.0)
-            };
-
-            CollectionAssert.AreEqual(results.Categories, expectedCategories, new CategoryLimitsEqualityComparer());
+            yield return new TestCaseData(
+                new AssessmentSection((Probability) 0.003, (Probability) 0.034),
+                new[]
+                {
+                    new AssessmentSectionCategory(EAssessmentGrade.APlus, (Probability) 0.0, (Probability) 0.0001),
+                    new AssessmentSectionCategory(EAssessmentGrade.A, (Probability) 0.0001, (Probability) 0.003),
+                    new AssessmentSectionCategory(EAssessmentGrade.B, (Probability) 0.003, (Probability) 0.034),
+                    new AssessmentSectionCategory(EAssessmentGrade.C, (Probability) 0.034, (Probability) 1.0),
+                    new AssessmentSectionCategory(EAssessmentGrade.D, (Probability) 1.0, (Probability) 1.0)
+                });
         }
     }
 }
