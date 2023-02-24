25df5d4cdee8c816c0d14eebb36825fca359add8
Robert Tangerman <robert.tangerman@deltares.nl>
2023-02-24 15:10:30 +0100

    Introduced boundary limits. ASK-86

---
+++ src/Assembly.Kernel/Model/BoundaryLimits.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Rijkswaterstaat 2022. All rights reserved.
+//
+// This file is part of the Assembly kernel.
+//
+// Assembly kernel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Rijkswaterstaat" are registered trademarks of
+// Rijkswaterstaat and remain full property of Rijkswaterstaat at all times.
+// All rights reserved.
+
+namespace Assembly.Kernel.Model
+{
+    /// <summary>
+    /// Class for boundary limits.
+    /// </summary>
+    public class BoundaryLimits : IHasBoundaryLimits
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="BoundaryLimits"/>.
+        /// </summary>
+        /// <param name="lowerLimit">The lower limit of the boundary.</param>
+        /// <param name="upperLimit">The upper limit of the boundary.</param>
+        public BoundaryLimits(Probability lowerLimit, Probability upperLimit)
+        {
+            LowerLimit = lowerLimit;
+            UpperLimit = upperLimit;
+        }
+
+        /// <inheritdoc />
+        public Probability LowerLimit { get; }
+
+        /// <inheritdoc />
+        public Probability UpperLimit { get; }
+    }
+}
---
+++ test/Assembly.Kernel.Test/BoundaryLimitsTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Rijkswaterstaat 2022. All rights reserved.
+//
+// This file is part of the Assembly kernel.
+//
+// Assembly kernel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Rijkswaterstaat" are registered trademarks of
+// Rijkswaterstaat and remain full property of Rijkswaterstaat at all times.
+// All rights reserved.
+
+using Assembly.Kernel.Model;
+using NUnit.Framework;
+
+namespace Assembly.Kernel.Test
+{
+    [TestFixture]
+    public class BoundaryLimitsTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var lowerLimit = new Probability(0.0);
+            var upperLimit = new Probability(1.0);
+
+            // Call
+            var boundaryLimits = new BoundaryLimits(lowerLimit, upperLimit);
+
+            // Assert
+            Assert.IsInstanceOf<IHasBoundaryLimits>(boundaryLimits);
+            Assert.AreEqual(lowerLimit, boundaryLimits.LowerLimit);
+            Assert.AreEqual(upperLimit, boundaryLimits.UpperLimit);
+        }
+    }
+}
